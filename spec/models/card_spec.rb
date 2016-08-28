e68dd5184049436beca0bbecc32fa4f245887084
Valera Tutaev <valeratutaev@gmail.com>
2016-08-28 17:08:50 +0300

    small change

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   describe '.Create_cards' do
-    it 'should have 52 cards' do
-      Card.delete_all
-      Card.create_cards
-      expect(Card.all.size).to eq 52
+    context '#create_cards' do
+      it 'should create 52 cards' do
+        Card.delete_all
+        Card.create_cards
+        expect(Card.all.size).to eq 52
+      end
     end
   end
-
 end
